0ac74469b27ce87797fc6842069dd781cd5a2833
murphy-li <murphy-li@foxmail.com>
2020-02-04 19:53:42 +0800

    fix #1 添加语调以及语速修改功能,添加国际化功能

--- app/src/main/java/org/murphy/MainActivity.java
+++ app/src/main/java/org/murphy/MainActivity.java
@@ -2,10 +2,14 @@ package org.murphy;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,12 +17,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
 
-    private TextView textView ;
+    private EditText editRate;
+    private EditText editPitch;
+    private Button testBt;
+    private Button applyBt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
+        editRate = findViewById(R.id.editRate);
+        editPitch = findViewById(R.id.editPitch);
+        testBt = findViewById(R.id.testBt);
+        applyBt = findViewById(R.id.applyBt);
         Intent startIntent = new Intent(MainActivity.this, MyService.class);
         //startService启动形式
         startService(startIntent);
@@ -30,5 +40,62 @@ public class MainActivity extends AppCompatActivity{
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_USER_PRESENT);
         registerReceiver(new MyReceiver(), filter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bindListener();
+    }
+
+    public void bindListener() {
+        testBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Did't implement now", Toast.LENGTH_LONG).show();
+            }
+        });
+        applyBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSpeechPitch(getSpeechPitch());
+                setSpeechRate(getSpeechRate());
+                Toast.makeText(MainActivity.this, R.string.OK, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public float getSpeechRate() {
+        if (editRate.getText() == null || "".equals(editRate.getText().toString())) {
+            return 0.0f;
+        }
+        return Float.parseFloat(editRate.getText().toString());
+    }
+
+    public void setSpeechRate(float rate) {
+        if (rate == 0.0f) {
+            return;
+        }
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.class.getName(), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("rate", rate);
+        editor.apply();
+    }
+
+    public float getSpeechPitch() {
+        if (editPitch.getText() == null || "".equals(editPitch.getText().toString())) {
+            return 0.0f;
+        }
+        return Float.parseFloat(editPitch.getText().toString());
+    }
+
+    public void setSpeechPitch(float pitch) {
+        if (pitch == 0.0f) {
+            return;
+        }
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.class.getName(), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("pitch", pitch);
+        editor.apply();
     }
 }
--- app/src/main/java/org/murphy/MyService.java
+++ app/src/main/java/org/murphy/MyService.java
@@ -2,7 +2,9 @@ package org.murphy;
 
 import android.app.Service;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
@@ -11,19 +13,49 @@ import android.widget.Toast;
 import java.util.Locale;
 import java.util.Objects;
 
-public class MyService extends Service implements TextToSpeech.OnInitListener {
+public class MyService extends Service {
+    private static final String LOG_TAG = MyService.class.getSimpleName();
     private ClipboardManager mClipboardManager;
     private ClipboardManager.OnPrimaryClipChangedListener mOnPrimaryClipChangedListener;
     private TextToSpeech mTextToSpeech;
+    private CharSequence content;
+    // 不懂为什么会调用两次，暂时先这么写
+    private long lastTime = 0;
+    private TextToSpeech.OnInitListener listener = new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+
+            if (status == TextToSpeech.SUCCESS) {
+                SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.class.getName(), Context.MODE_PRIVATE);
+                // 设置音调,1.0是常规， 大是女声，小是男声，1.2声音还不错！
+                float pitch = sharedPreferences.getFloat("pitch", 1.2f);
+                //设定语速 ，默认1.0正常语速
+                float speechRate = sharedPreferences.getFloat("rate", 0.7f);
+                mTextToSpeech.setPitch(pitch);
+                mTextToSpeech.setSpeechRate(speechRate);
+
+                int result = mTextToSpeech.setLanguage(Locale.CHINA);
+                Log.i(LOG_TAG, "TextToSpeech status is " + status);
+                if (result == TextToSpeech.LANG_MISSING_DATA
+                        || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(MyService.this, R.string.set_TTS, Toast.LENGTH_LONG).show();
+                } else if (result > 0) {
+//                    Toast.makeText(MyService.this, R.string.initialization_succeessful, Toast.LENGTH_LONG).show();
+                    speak();
+                } else {
+                    Toast.makeText(MyService.this, R.string.unknow_error_and_staus_code_is + status, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
+
     @Override
-    public IBinder onBind(Intent intent)
-    {
+    public IBinder onBind(Intent intent) {
         return null;
     }
+
     @Override
-    public void onCreate()
-    {
-        mTextToSpeech = new TextToSpeech(this, this);
+    public void onCreate() {
         registerClipEvents();
     }
 
@@ -32,16 +64,24 @@ public class MyService extends Service implements TextToSpeech.OnInitListener {
      */
     private void registerClipEvents() {
         mClipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-        mOnPrimaryClipChangedListener  = new ClipboardManager.OnPrimaryClipChangedListener() {
+        mOnPrimaryClipChangedListener = new ClipboardManager.OnPrimaryClipChangedListener() {
+            // TODO I don't now why this method always called twice
             @Override
             public void onPrimaryClipChanged() {
+                long now = System.currentTimeMillis();
+                if (now - lastTime < 1000) {
+                    lastTime = now;
+                    return;
+                }
+                lastTime = now;
                 if (mClipboardManager.hasPrimaryClip()
                         && Objects.requireNonNull(mClipboardManager.getPrimaryClip()).getItemCount() > 0) {
                     // 获取复制、剪切的文本内容
                     CharSequence content =
                             mClipboardManager.getPrimaryClip().getItemAt(0).getText();
-                    Log.d("TAG", "复制、剪切的内容为：" + content);
-                    speak(content);
+                    Log.d(LOG_TAG, "The content of clipboard: " + content);
+                    mTextToSpeech = new TextToSpeech(MyService.this, listener);
+                    MyService.this.content = content;
                 }
             }
         };
@@ -50,38 +90,15 @@ public class MyService extends Service implements TextToSpeech.OnInitListener {
 
     /**
      * 说
-     * @param text
      */
-    public void speak(CharSequence text) {
-        if (!mTextToSpeech.isSpeaking()) {
-//            Bundle bundle = new Bundle();
-//            bundle.putFloat(TextToSpeech.Engine.KEY_PARAM_VOLUME,1.0f);
-            //朗读，注意这里三个参数的added in API level 4   四个参数的added in API level 21
-//            mTextToSpeech.speak(mBinding.editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
-            mTextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "");
-        }
-    }
-    /**
-     * 初始化语音播放
-     * @param status
-     */
-    @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS){
-            // 设置音调,1.0是常规
-            mTextToSpeech.setPitch(1.0f);
-            //设定语速 ，默认1.0正常语速
-            mTextToSpeech.setSpeechRate(1.0f);
-            int result = mTextToSpeech.setLanguage(Locale.CHINA);
-            Log.i("status", result + "");
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED){
-                Toast.makeText(this, "请设置Google TTS", Toast.LENGTH_LONG).show();
-            }else if(result > 0){
-                Toast.makeText(this, "初始化成功，请将软件加到保护名单里面，不然会被杀掉！", Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(this, "未知错误，状态码为：" + status, Toast.LENGTH_LONG).show();
+    public void speak() {
+        if (mTextToSpeech != null) {
+            if (mTextToSpeech.isSpeaking()) {
+                mTextToSpeech.stop();
             }
+            mTextToSpeech.speak(this.content, TextToSpeech.QUEUE_FLUSH, null, "");
+        } else {
+            Toast.makeText(this, R.string.text_to_speak_is_not_work_properly, Toast.LENGTH_LONG).show();
         }
     }
 
@@ -95,6 +112,9 @@ public class MyService extends Service implements TextToSpeech.OnInitListener {
             mClipboardManager.removePrimaryClipChangedListener(mOnPrimaryClipChangedListener);
             mClipboardManager = null;
         }
-        mTextToSpeech = null;
+        if (mTextToSpeech != null) {
+            mTextToSpeech.shutdown();
+            mTextToSpeech = null;
+        }
     }
 }
